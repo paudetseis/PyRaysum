3497c313ed97cbd05ed3b8ddb6b6b41029e0ea1a
Pascal Audet <pascal.audet@uottawa.ca>
2020-12-02 15:50:34 -0500

    new install options and instructions

--- README.md
+++ README.md
@@ -7,12 +7,71 @@ of a stack of layers with planar but nonparallel (dipping) interfaces,
 allowing the possibility of anisotropy in the layers. Incident P and S
 waves are supported.
 
+Although a trimmed down version of the original Fortran code is supplied with `PyRaysum`, you can find the original version [here](https://home.cc.umanitoba.ca/~frederik/Software/).
+
+### Dependencies
+
+`PyRaysum` requires a modern Fortran compiler (e.g., gfortran, ifort). In addition, the following packages are required:
+
+- [pandas](https://pandas.pydata.org)
+- [obspy](https://docs.obspy.org)
+
+### Installing
+
+Documentation for `PyRaysum` is in development and this Readme file provides minimal guidance on how to install and run the code. To install `PyRaysum` using `conda`, you can first create a `conda` environment and install the dependencies at the same time:
+
+```
+conda create -n prs python=3.8 obspy pandas fortran-compiler -c conda-forge
+conda activate prs
+```
+
+Then clone and install `PyRaysum` using `pip`:
+
+```
+git clone https://github.com/paudetseis/PyRaysum.git
+cd PyRaysum
+pip install .
+```
+
+After `PyRaysum` is installed, you need to compile and install the Raysum binaries by running the provided `install-raysum` script. Check out the helper page for the available options:
+
+```
+install-raysum -h
+```
+
+Running the script without arguments will attempt to install Raysum to default paths and fortran compiler (these are contained in the $PATH and $FC environment variables). If you installed `PyRaysum` using `conda` (and the `fortran-compiler` package), you can safely ignore the options and run the script without any options. To call the script with options (which take precedence over any default `conda` environment variables), you would specify:
+
+```
+install-raysum --path=/path/to/binaries/bin --fcompiler=gfortran
+```
+
+Note that the `--path` option requires the full, absolute path. If you specify a path in one of the root directories, you may have to run `install-raysum` with super-user privileges, for example:
+
+```
+sudo install-raysym --path=/usr/bin
+```
+
+### Notebooks
+
+There is currently one notebook bundled with `PyRaysum`, which reproduces some published examples in [Porter et al., 2011](https://doi.org/10.1130/L126.1). To download it locally and run it, first install `jupyter`:
+
+```
+conda install jupyter
+```
+
+Then open a `Python` window and type:
+
+```
+from pyraysum import doc
+doc.install_doc(path='Notebooks')
+```
+
+which will install the notebook locally in the folder `'Notebooks'`. Then you simply open a `jupyter notebook` and navigate to the `Notebook` folder and open the corresponding file. 
+
 #### Citing
 
-If you use `PyRaysum` in your work, please cite the Zenodo DOI
-and the following paper:
+If you use `PyRaysum` in your work, please cite the Zenodo DOI (not yet available) and the following paper:
 
 - Frederiksen, A.W., and Bostock, M.G. (1999) Modelling teleseismic waves in dipping anisotropic structures. Geophysical Journal International 141: 401-412.
   
-The original Fortran version of the code
-is available [here](https://home.cc.umanitoba.ca/~frederik/Software/).
+
--- pyraysum/scripts/install_raysum.py
+++ pyraysum/scripts/install_raysum.py
@@ -3,22 +3,77 @@ import subprocess
 import pyraysum
 from pathlib import Path
 from pyraysum import src
+from argparse import ArgumentParser
 
 
-def main():
+def get_install_args(argv=None):
 
-    # Find out if conda is installed
+    parser = ArgumentParser(
+        usage="%(prog)s [options]",
+        description="Script used to compile the Raysum Fortran modules " +
+        "and install them locally on the system. The script will attempt " +
+        "to use default paths, or you can specify the path where you want " +
+        "the binaries to be installed. You can also specify the Fortran " +
+        "compiler of your choice that exists on your system. If PyRaysum " +
+        "was installed using conda with the conda-provided fortran " +
+        "compiler, you can safely ignore these options. However, specifying " +
+        "these options will take precedence over the conda defaults.")
+    parser.add_argument(
+        "--path",
+        action="store",
+        type=str,
+        dest="install_path",
+        default="",
+        help="Destination where the Fortran binaries will be installed. " +
+        "Use absolute path only - the install may fail otherwise. Check " +
+        "your $PATH environment variable to find out what these are. " +
+        "If your path is in the root directory, you may have to run this " +
+        "script with super user privileges.")
+    parser.add_argument(
+        "--fcompiler",
+        action="store",
+        type=str,
+        dest="fcompiler",
+        default="gfortran",
+        help="Fortran compiler used to produce binaries for Raysum. " +
+        "For example, --fcompiler=gfortran or --fcompiler=ifort. Check " +
+        "your $FC environment variable to find out if you have a Fortran " +
+        "compiler installed.")
+
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def main(args=None):
+
+    if args is None:
+        args = get_install_args()
+
     conda_exists = os.getenv("CONDA_PREFIX")
 
-    # Check out where binaries will be located
-    if conda_exists:
-        prs_path = Path(os.getenv("CONDA_PREFIX")) / "bin"
+    # If user specifies path, use it
+    if args.install_path:
+        prs_path = Path(args.install_path)
+
+    # Otherwise, attempt to find suitable path
     else:
-        prs_path = Path(os.getenv("PATH").split(":")[0])
+        # First find out if conda is installed
+        if conda_exists:
+            prs_path = Path(os.getenv("CONDA_PREFIX")) / "bin"
+        # Conda not installed - use first path in $PATH environment
+        else:
+            prs_path = Path(os.getenv("PATH").split(":")[0])
+
+    # If user specifies fortran compiler, use it
+    if args.fcompiler:
+        os.environ["FC"] = args.fcompiler
 
     # Print message to screen where binaries are installed
     print()
     print("*"*len(str(prs_path)))
+    print()
+    print("Fortran compiler is : \n"+str(os.getenv("FC")))
     print()
     print("Raysum will be installed in : \n"+str(prs_path))
     print()
@@ -28,6 +83,8 @@ def main():
     # Change directory to where the Fortran source files are located
     src_path = src.__path__[0]
     os.chdir(src_path)
+
+    # Set FC environment variable
 
     # Compile Raysum using the Makefile: clean, compile, copy binaries, clean
     subprocess.call(["make", "clean"])
