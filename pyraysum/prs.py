2f4613e7c172a340a672efe0ffc5f80347a8a08a
Wasja Bloch <wbloch@eoas.ubc.ca>
2022-04-04 13:07:21 -0700

    Model.cobine_layers() now Model.average_layers(). Python indexing

--- pyraysum/prs.py
+++ pyraysum/prs.py
@@ -333,16 +333,16 @@ class Model(object):
         self.nlay -= 1
         self.update()
 
-    def combine_layers(self, top, bottom):
+    def average_layers(self, top, bottom):
         """
         Combine layers between top and bottom index into one with summed
         thicknes and average vp, vs, and rho.
 
         Args:
             top : (int)
-                Index of topmost layer to include in combination
+                Index before topmost layer to include in combination
             bottom : (int)
-                Index of bottommost layer to include in combination
+                Index after bottommost layer to include in combination
 
         Raises:
             IndexError if bottom less or equal top
@@ -352,24 +352,22 @@ class Model(object):
         if bottom <= top:
             raise IndexError('bottom must be larger than top.')
 
-        bot = bottom + 1
-
-        if not all(self.flag[top:bot]):
+        if not all(self.flag[top:bottom]):
             raise ValueError('Can only combine isotropic layers')
 
-        if not all(self.dip[top:bot][0] == self.dip[top:bot]):
+        if not all(self.dip[top:bottom][0] == self.dip[top:bottom]):
             raise ValueError('All layers must have the same dip')
 
-        if not all(self.strike[top:bot][0] == self.strike[top:bot]):
+        if not all(self.strike[top:bottom][0] == self.strike[top:bottom]):
             raise ValueError('All layers must have the same strike')
 
-        thickn = sum(self.thickn[top:bot])
-        weights = self.thickn[top:bot] / thickn
+        thickn = sum(self.thickn[top:bottom])
+        weights = self.thickn[top:bottom] / thickn
 
         layer = {'thickn': thickn,
-                 'vp': sum(self.vp[top:bot] * weights),
-                 'vs': sum(self.vs[top:bot] * weights),
-                 'rho': sum(self.rho[top:bot] * weights)}
+                 'vp': sum(self.vp[top:bottom] * weights),
+                 'vs': sum(self.vs[top:bottom] * weights),
+                 'rho': sum(self.rho[top:bottom] * weights)}
 
         for att in self._useratts:
             try:
@@ -377,9 +375,9 @@ class Model(object):
             except KeyError:
                 pass
             self.__dict__[att] = np.delete(self.__dict__[att],
-                                           range(top+1, bot))
+                                           range(top+1, bottom))
 
-        self.nlay -= bottom - top
+        self.nlay -= bottom - top - 1
         self.update()
 
     def times(self):
