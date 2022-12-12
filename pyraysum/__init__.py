c256ecdd064382a65bf0985863573a14fbd905b8
Wasja Bloch <wbloch@eoas.ubc.ca>
2022-12-11 20:11:36 -0800

    version 0.3.2

--- pyraysum/__init__.py
+++ pyraysum/__init__.py
@@ -20,6 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 from .prs import Model, Geometry, Control, Result, run
